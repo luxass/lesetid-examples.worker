30146a893be01469e4562b264d654e23b416245a
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-10-28 21:20:00 +0200

    chore: testing

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,8 @@ import type { Context } from "hono";
 import type { Example } from "./types";
 
 export function addExampleRedirects<TContext extends Context>(ctx: TContext, url: URL, example: string) {
+  console.info("addExampleRedirects", example, url);
+
   if (url.host.startsWith(`${example}-example`)) {
     if (url.pathname === "/codesandbox") {
       return ctx.redirect(`/${example}/codesandbox`);
